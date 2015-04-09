dc4bb43f991a97c01e9af9e346585fdbed111d29
Agusti Bau <agustibau@gmail.com>
2015-04-09 14:44:31 +0200

    tests

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        DriverGraf DG = new DriverGraf();
-        DG.main();
-    }
-}
---
+++ src/Test.java
@@ -0,0 +1,34 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        Graf<String> testGraf = new Graf<String>();
+        testGraf.afegirNode("1");
+        testGraf.afegirNode("2");
+        testGraf.afegirNode("3");
+        testGraf.afegirNode("4");
+        System.out.println("ordre Graf:" + testGraf.ordre() + " :  4");
+
+        testGraf.afegirArc("1", "2", 1);
+        testGraf.afegirArc("2", "1", 1);
+        System.out.println("Mida Graf:" + testGraf.mida() + " :  2");
+
+        try {
+            testGraf.afegirArc("1", "2", 1);
+            System.out.println("FUCK");
+        }catch(RuntimeException ex){
+            System.out.println(ex.getLocalizedMessage());
+        }
+
+        System.out.println("tostring preBorrat:" + testGraf.toString());
+
+        testGraf.eliminarArc("1", "2");
+        System.out.println("Mida Graf:" + testGraf.mida() + " :  1");
+
+        testGraf.eliminarNode("1");
+        System.out.println("ordre Graf:" + testGraf.ordre() + " :  3");
+
+        System.out.println("tostring postBorrat:" + testGraf.toString());
+
+    }
+}
